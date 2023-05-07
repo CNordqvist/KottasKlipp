2011d2a9e4d8f8562d7c9a670882ba2c16e1cef4
CNordqvist <christoffer.nord93@gmail.com>
2023-05-07 14:41:02 +0200

    first commit

---
+++ site.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        name: "Damklippning",
+        description: "är du en dam och vill bli klippt? denna är för dig!",
+        price: 250.00,
+        image: "./Assets/Images/Women.webp",
+    },
+    {
+        name: "Herrklippning",
+        description: "är du en herre och vill bli klippt? denna är för dig!",
+        price: 200.00,
+        image: "./Assets/Images/Men.jpg",
+    },
+    {
+        name: "Barnklippning",
+        description: "är du ett barn och dina föräldrar vill att du ska klippa dig? denna är för dig!",
+        price: 100.00,
+        image: "./Assets/Images/Kids.jpg",
+    },
+    {
+        name: "Hårfärning",
+        description: "Vill du byta hårfärg? Kom till oss!",
+        price: 150.00,
+        image: "./Assets/Images/Dye.webp",
+    }
+]
+
+console.log(products)
+
